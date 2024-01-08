3721a7c406df9c4f4dea5d4c6b90d9de16e248e6
amorygalili <amorygalili@gmail.com>
2024-01-08 12:38:31 -0500

    Updated dashboard

--- dashboard/src/index.ts
+++
@@ -1,6 +0,0 @@
-import createDashboard from "@frc-web-components/frc-web-components";
-
-window.addEventListener("DOMContentLoaded", () => {
-  const dashboard = createDashboard(document.body);
-  (window as any).dasboard = dashboard;
-});
